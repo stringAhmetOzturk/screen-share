c1fe7532d0343d177223d64fae0c4a0a2b099359
Ahmet Öztürk <95494301+stringAhmetOzturk@users.noreply.github.com>
2023-10-23 17:30:41 +0300

    Create allowedOrigins.js

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+export const allowedOrigins = [
+	'https://localhost:5000',
+	'https://85.214.227.222:5000',
+	'https://ml.code-vet-project.eu:5000',
+	'http://85.214.227.222:5000',
+	'http://ml.code-vet-project.eu:5000'
+	]
+
